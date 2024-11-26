37ae9a6445f933caaca8001d3e77c59aebbd77e7
StalicJi <a7330831@gmail.com>
2024-11-26 23:28:56 +0800

    random css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={createGenerateClassName()}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
